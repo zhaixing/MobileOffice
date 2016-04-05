dacc87789128f6d8eb26b866e51f5ad26cb7c1f4
ultracpy <674917570@qq.com>
2016-04-05 19:09:22 +0800

    Initial Commit

---
+++ OD01/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OD01
+//
+//  Created by sam on 16/4/5.
+//  Copyright © 2016年 sam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ OD01/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  OD01
+//
+//  Created by sam on 16/4/5.
+//  Copyright © 2016年 sam. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
