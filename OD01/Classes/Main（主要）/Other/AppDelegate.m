b6a338d445f6ae59b0d4472c44d2327527b3b51a
ultracpy <674917570@qq.com>
2016-04-07 15:41:29 +0800

    23-版本新特性03-立即体验按钮已经完成，判断的注释也取消了。

--- OD01/Classes/Main（主要）/Other/AppDelegate.m
+++ OD01/Classes/Main（主要）/Other/AppDelegate.m
@@ -30,33 +30,33 @@
     /**
      *  aaaaaaaaaaaaaaaaa
      */
-//    //如何知道第一次使用这个版本？比较上次的使用情况
-//    NSString *versionKey=@"CFBundleVersion";
-//    
-//    //从沙盒中取出上次存的版本号（去出上次用户的使用记录）
-//    NSUserDefaults *defaults=[NSUserDefaults standardUserDefaults];
-//    NSString *lastVersion=[defaults objectForKey:versionKey];
-//    
-//    //获得当前打开软件的版本号
-//    NSString *currentVersion=[NSBundle mainBundle].infoDictionary[versionKey];
-//    
-//    if ([currentVersion isEqualToString:lastVersion]) {//当前版本号==上次版本号：显示HMTabBarViewController
-//        self.window.rootViewController=[[HMTabBarViewController alloc] init];
-//    }else{//当前版本号！=上次使用的版本：显示新特性
-//        self.window.rootViewController=[[HMNewfeatureViewController alloc] init];
-//        
-//        //存储这次使用的软件版本
-//        [defaults setObject:currentVersion forKey:versionKey];
-//        [defaults synchronize  ];
-//    }
-//    
-////    self.window.rootViewController=[[HMTabBarViewController alloc] init];
-////    self.window.rootViewController.view.tintColor =[UIColor blackColor];
+    //如何知道第一次使用这个版本？比较上次的使用情况
+    NSString *versionKey=@"CFBundleVersion";
+    
+    //从沙盒中取出上次存的版本号（去出上次用户的使用记录）
+    NSUserDefaults *defaults=[NSUserDefaults standardUserDefaults];
+    NSString *lastVersion=[defaults objectForKey:versionKey];
+    
+    //获得当前打开软件的版本号
+    NSString *currentVersion=[NSBundle mainBundle].infoDictionary[versionKey];
+    
+    if ([currentVersion isEqualToString:lastVersion]) {//当前版本号==上次版本号：显示HMTabBarViewController
+        self.window.rootViewController=[[HMTabBarViewController alloc] init];
+    }else{//当前版本号！=上次使用的版本：显示新特性
+        self.window.rootViewController=[[HMNewfeatureViewController alloc] init];
+        
+        //存储这次使用的软件版本
+        [defaults setObject:currentVersion forKey:versionKey];
+        [defaults synchronize  ];
+    }
+    
+//    self.window.rootViewController=[[HMTabBarViewController alloc] init];
+//    self.window.rootViewController.view.tintColor =[UIColor blackColor];
     
     /**
      *  bbbbbbbbbb
      */
-    self.window.rootViewController=[[HMNewfeatureViewController alloc] init];
+//    self.window.rootViewController=[[HMNewfeatureViewController alloc] init];
     
     
     //添加自控制器
--- OD01/Classes/Newfeature(新特性)/Controller/HMNewfeatureViewController.m
+++ OD01/Classes/Newfeature(新特性)/Controller/HMNewfeatureViewController.m
@@ -10,6 +10,7 @@
 #import "HMGlobal.h"
 #import "UIImage+Extension.h"
 #import "UIView+Extension.h"
+#import "HMTabBarViewController.h"
 
 #define HMNewfeatureImageCount 5
 
@@ -58,13 +59,16 @@
         imageView.height=imageH;
         imageView.x=i*imageW;
         
-        //设置其他属性
-        scrollView.contentSize=CGSizeMake(HMNewfeatureImageCount*imageW, 0);//可以向右滑动了
-        scrollView.pagingEnabled=YES;//
-        scrollView.showsHorizontalScrollIndicator=NO;//去掉水平控制条
-        scrollView.backgroundColor=[UIColor colorWithRed:106/255.0 green:198/255.0 blue:78/255.0 alpha:1.0];//左边的弹性设置
+        //给最后一个view添加“立即体验”按钮
+        if(i==HMNewfeatureImageCount-1){
+            [self setupLastImageView:imageView];
+        }
     }
-    
+    //设置其他属性
+    scrollView.contentSize=CGSizeMake(HMNewfeatureImageCount*imageW, 0);//可以向右滑动了
+    scrollView.pagingEnabled=YES;//
+    scrollView.showsHorizontalScrollIndicator=NO;//去掉水平控制条
+    scrollView.backgroundColor=[UIColor colorWithRed:106/255.0 green:198/255.0 blue:78/255.0 alpha:1.0];//左边的弹性设置
     
 }
 
@@ -87,4 +91,53 @@
 //    pageControl.pageIndicatorTintColor=themeColor;//非当前页
 //    
 //}
+
+/**
+ *  设置最后一个UIImageView的内容
+ *
+ *  @param imageView <#imageView description#>
+ */
+-(void)setupLastImageView:(UIImageView *)imageView
+{
+    imageView.userInteractionEnabled=YES;
+    //添加开始按钮
+    [self setupStartButton:imageView];
+}
+
+/**
+ *  开始按钮
+ */
+-(void)setupStartButton:(UIImageView *)imageView
+{
+    //1.添加开始按钮
+    UIButton *startButton=[[UIButton alloc] init];
+    [imageView addSubview:startButton];
+    
+    //设置背景图片
+//    [startButton setBackgroundColor:[UIColor ]];
+    //设置frame
+    startButton.frame=CGRectMake(self.view.width*0.22, self.view.height*0.84, 180, 38);
+    //设置文字
+//    [startButton setTitle:@"开始" forState:UIControlStateNormal];
+    [startButton addTarget:self action:@selector(start) forControlEvents:UIControlEventTouchUpInside];
+}
+
+/**
+ *  start按钮实现
+ */
+-(void)start
+{
+//    NSLog(@"start---");
+    //显示主窗口
+    //三种方法
+    
+    //1.push
+    
+    //2.model
+    HMTabBarViewController *vc=[[HMTabBarViewController alloc] init];
+    [self presentViewController:vc animated:NO completion:nil];
+    
+    //3.window.rootViewController
+}
 @end
+
