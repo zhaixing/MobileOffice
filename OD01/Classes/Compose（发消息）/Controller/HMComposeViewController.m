4e45a1f6e1d79808e81c701c23ac7bc901a3aca9
ultracpy <674917570@qq.com>
2016-04-09 16:00:47 +0800

    36-首页07-自定义UITextView，即写消息的地方

--- OD01/Classes/Compose（发消息）/Controller/HMComposeViewController.m
+++ OD01/Classes/Compose（发消息）/Controller/HMComposeViewController.m
@@ -8,14 +8,46 @@
 
 #import "HMComposeViewController.h"
 #import "HMGlobal.h"
-@interface HMComposeViewController ()
+#import "HMTextView.h"
 
+@interface HMComposeViewController ()
+@property (nonatomic, weak) HMTextView *textView;
 @end
 
 @implementation HMComposeViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    //添加导航栏按钮
+    [self setNavBar];
+    
+    //添加输入控件UITextView
+    [self setupTextView];
+    
+}
+
+/**
+ *  添加输入控件UITextView 的实现
+ */
+-(void)setupTextView
+{
+    //1.创建控件
+    HMTextView *textView=[[HMTextView alloc] init];
+    textView.frame=self.view.bounds;
+    [self.view addSubview:textView];
+    self.textView=textView;
+    
+    //2.设置提醒文字（站位文字）
+    textView.placehoder=@"分享到同事圈...";
+    //3.设置字体
+    textView.font=[UIFont systemFontOfSize:15];
+}
+/**
+ *  添加导航栏按钮的实现
+ */
+-(void)setNavBar
+{
     self.title=@"写消息";
     self.view.backgroundColor=[UIColor whiteColor];
     self.navigationItem.leftBarButtonItem=[[UIBarButtonItem alloc] initWithTitle:@"取消" style:UIBarButtonItemStylePlain target:self action:@selector(cancel)];
---
+++ OD01/Classes/Home（首页）/View/HMTextView.h
@@ -0,0 +1,14 @@
+//
+//  HMTextView.h
+//  猫头鹰助理
+//
+//  Created by sam on 16/4/9.
+//  Copyright © 2016年 sam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HMTextView : UITextView
+@property (nonatomic, copy) NSString *placehoder;
+@property (nonatomic, strong) UIColor *placehoderColor;
+@end
